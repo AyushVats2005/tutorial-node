9bb7a21a8b5f3d99c668bb24e11695c65afbfcff
AyushVats2005 <vatsayush2005@gmail.com>
2026-06-23 12:46:50 +0530

    First Commit

---
+++ 07-mind-genade.js
@@ -0,0 +1,8 @@
+const num1 = 5
+const num2 = 10
+
+function addValues() {
+    console.log(`the sum is :${num1 + num2}`)
+}
+
+addValues()//once required will only run and execute and no return value since no export is done.
